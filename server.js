f52d0fd8387577f78c0cb0fad748a5542b828e27
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-02 18:39:29 -0600

    basic folders have been created

---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// Initializes Sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const { strict } = require('assert');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up session and connect to our Sequelize db
+const sess = {
+  secret: 'Super secret secret',
+  // TODO: Add a comment describing the purpose of adding a cookies object to our options to our session object
+  cookie: {
+    // TODO: Add a comment describing the functionality of the maxAge attribute
+    // Stored in milliseconds
+    // maxAge seets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. 
+    maxAge: 60 * 60 * 1000, // expires after 1 hr
+    // TODO: Add a comment describing the functionality of the httpOnly attribute
+    // If it is an http only cookie, this mean client side javascript can't access it.
+    httpOnly: true,
+    // TODO: Add a comment describing the functionality of the secure attribute
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // TODO: Add a comment describing the functionality of the sameSite attribute
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
