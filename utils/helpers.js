7b53ad4d72f9ddaa50ebd5504c2d86fb46ce0b0a
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-10 15:39:08 -0600

    add comment has been included

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,6 +7,13 @@ module.exports = {
       new Date(date).getFullYear() + 5
     }`;
   },
+  
+  /* This will return given word's plural, e.g.,
+    if 'comment' is given & if comments.length is != 1, it will return: 
+      'comments'
+    otherwise, it will return:
+      'comment'
+  */
   format_plural: (word, amount) => {
     if (amount !== 1) {
       return `${word}s`;
